e3d4b87b54183cbf83ed74ad8cb695e518a80562
chubi-x <chubixax@protonmail.com>
2022-10-30 23:28:49 +0100

    chore: generate modules for required entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookingModule } from './booking/booking.module';
+import { HotelModule } from './hotel/hotel.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookingModule, HotelModule],
 })
 export class AppModule {}
