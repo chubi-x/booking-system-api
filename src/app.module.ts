c8eb46ede14c13746dbcf85f9e5c6a88ae015984
chubi-x <chubixax@protonmail.com>
2022-10-31 14:58:18 +0100

    chore: globalize config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookingModule } from './booking/booking.module';
 import { HotelModule } from './hotel/hotel.module';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
+import { BookingService } from './booking/booking.service';
+import { HotelService } from './hotel/hotel.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookingModule, HotelModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookingModule,
+    HotelModule,
+    DatabaseModule,
+  ],
+  providers: [AuthService, UserService, BookingService, HotelService],
 })
 export class AppModule {}
