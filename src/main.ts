5db568f4fbf8c5f28badc1336f7ba1b98089e9f9
chubi-x <chubixax@protonmail.com>
2022-11-01 13:08:01 +0100

    feat: add session functionality

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import dotenv from 'dotenv';
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidUnknownValues: true,
+    }),
+  );
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
     }),
   );
   await app.listen(4000);
