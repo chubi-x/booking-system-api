3547c3fb0bbd404eb4a225d4b338071f430847f2
chubi-x <chubixax@protonmail.com>
2022-11-01 16:47:17 +0100

    chore: flesh out updatebio dto

--- src/user/dto/update-bio.dto.ts
+++ src/user/dto/update-bio.dto.ts
@@ -1,1 +1,41 @@
-export class UpdateBioDto {}
+import {
+  IsString,
+  IsOptional,
+  IsUrl,
+  IsDate,
+  IsPhoneNumber,
+} from 'class-validator';
+export class UpdateBioDto {
+  @IsString()
+  @IsOptional()
+  firstName?: string;
+
+  @IsString()
+  @IsOptional()
+  lastName?: string;
+
+  @IsString()
+  @IsUrl()
+  @IsOptional()
+  profilePicture?: string;
+
+  @IsDate()
+  @IsOptional()
+  dateOfBirth?: Date;
+
+  @IsPhoneNumber()
+  @IsOptional()
+  phoneNumber?: number;
+
+  @IsString()
+  @IsOptional()
+  gender?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+
+  @IsString()
+  @IsOptional()
+  nationality?: string;
+}
