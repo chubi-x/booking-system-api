ff38f111da8ca7e6501d856fd52e9140ad21ea47
chubi-x <chubixax@protonmail.com>
2022-11-01 13:17:23 +0100

    chore: resolve dotenv import

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
-import dotenv from 'dotenv';
-
+import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
