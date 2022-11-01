d01c2b4cbc1e9c1c66d0fe3c54789ce9443af374
chubi-x <chubixax@protonmail.com>
2022-11-01 16:30:13 +0100

    chore: make auth guard global

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,22 @@
-import { Controller, Get, Res, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Res, Req, UseGuards, Patch } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AuthGuard } from '../guards';
 import { UserService } from './user.service';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
   // get user
-  @UseGuards(AuthGuard)
   @Get('')
   async getUser(@Req() req: Request, @Res() res: Response) {
-    return this.userService.getUser(req, res);
+    return await this.userService.getUser(req, res);
   }
   // update user
+  @Patch('')
+  async editUser() {
+    return await this.userService.editUser();
+  }
   // verify account
   // reset password
   // delete user
