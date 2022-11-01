30a44d3d8d8d4aaf32f7699f19f5edc42f6434d4
chubi-x <chubixax@protonmail.com>
2022-11-01 14:59:09 +0100

    feat: implement get user route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, Req, UseGuards } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthGuard } from '../guards';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+  // get user
+  @UseGuards(AuthGuard)
+  @Get('')
+  async getUser(@Req() req: Request, @Res() res: Response) {
+    return this.userService.getUser(req, res);
+  }
+  // update user
+  // verify account
+  // reset password
+  // delete user
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { PrismaService } from '../database/database.service';
+import { HelpersService } from '../helpers/helpers.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private prisma: PrismaService,
+    private resHandler: HelpersService.ResponseHandler,
+  ) {}
+
+  async getUser(req: Request, res: Response) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: req.session.userId,
+      },
+    });
+    return this.resHandler.requestSuccessful({ res, payload: { ...user } });
+  }
+}
