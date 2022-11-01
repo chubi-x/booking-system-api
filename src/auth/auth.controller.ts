5d7e6ce0839d9254ca9d358780e7023ee1a0c093
chubi-x <chubixax@protonmail.com>
2022-11-01 13:27:36 +0100

    feat: implement login logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, Session } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
-import { SignupDto } from './dto';
+import { LoginDto, SignupDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +10,13 @@ export class AuthController {
   @Post('signup')
   async signup(@Body() dto: SignupDto, @Res() res: Response) {
     return await this.authService.signup(dto, res);
+  }
+  @Post('login')
+  async login(
+    @Body() dto: LoginDto,
+    @Res() res: Response,
+    @Session() session: Record<string, any>,
+  ) {
+    return await this.authService.login(dto, res, session);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { Response } from 'express';
 import { PrismaService } from '../database/database.service';
 import { HelpersService } from '../helpers/helpers.service';
-import { SignupDto } from './dto';
+import { LoginDto, SignupDto } from './dto';
 @Injectable()
 export class AuthService {
   constructor(
@@ -13,22 +13,58 @@ export class AuthService {
 
   async signup(dto: SignupDto, res: Response) {
     const password = await argon.hash(dto.password);
+    // first check if user exists
+
     try {
-      await this.prisma.user.create({
-        data: {
-          firstName: dto.firstName,
-          lastName: dto.lastName,
+      const user = await this.prisma.user.findUnique({
+        where: {
           email: dto.email,
-          password,
         },
       });
+      if (!user) {
+        await this.prisma.user.create({
+          data: {
+            firstName: dto.firstName,
+            lastName: dto.lastName,
+            email: dto.email,
+            password,
+          },
+        });
+        return this.resHandler.requestSuccessful({
+          res,
+          message: 'User created successfully',
+          status: 201,
+        });
+      } else {
+        return this.resHandler.clientError(res, 'User already exists', 400);
+      }
+    } catch (err) {
+      return this.resHandler.serverError(res, 'Error creating user');
+    }
+  }
+  async login(dto: LoginDto, res: Response, session: Record<string, any>) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+      if (!user) {
+        return this.resHandler.clientError(res, 'User does not exist', 400);
+      }
+      const passwordMatches = argon.verify(user.password, dto.password);
+      if (!passwordMatches) {
+        return this.resHandler.clientError(res, 'Incorrect password', 400);
+      }
+      // save user session
+      session.userId = user.id;
       return this.resHandler.requestSuccessful({
         res,
-        message: 'User created successfully',
-        status: 201,
+        message: 'Login successful',
+        status: 200,
       });
     } catch (err) {
-      this.resHandler.serverError(res, 'Error creating user');
+      return this.resHandler.serverError(res, 'Error logging in');
     }
   }
 }
