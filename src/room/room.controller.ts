881c9b601559184abc1dbb0844f839e409560022
chubi-x <chubixax@protonmail.com>
2022-11-03 00:46:17 +0100

    feat: implement logic to create new room

--- src/room/room.controller.ts
+++ src/room/room.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Body,
+  Patch,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { RoomService } from './room.service';
-
-@Controller('room')
+import { GetHotel } from '../decorators';
+import { HotelAuthGuard } from '../guards';
+import { NewRoomDto } from './dto';
+@Controller('hotels/rooms')
 export class RoomController {
   constructor(private roomService: RoomService) {}
+
+  // create hotel room (using logged in hotel )
+  @UseGuards(HotelAuthGuard)
+  @Post('/new')
+  async newRoom(
+    @GetHotel() hotelId: string,
+    @Body() dto: NewRoomDto,
+    @Res() res: Response,
+  ) {
+    return this.roomService.newRoom(hotelId, dto, res);
+  }
+  //update room details //PATCH
+  //  get all rooms by single hotel (using logged in hotel ) GET
+  // get all rooms (public) GET
+  // delete room (using logged in hotel ) DELETE
 }
--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,7 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
 import { PrismaService } from '../database/database.service';
+import { HelpersService } from '../helpers/helpers.service';
+import { NewRoomDto } from './dto';
 
 @Injectable()
 export class RoomService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private resHandler: HelpersService.ResponseHandler,
+  ) {}
+
+  /**
+   *
+   * @param hotelId Hotel's Id
+   * @param dto class containing new room details
+   * @param res Express Response Object
+   * @returns ResponseHandler
+   */
+  async newRoom(hotelId: string, dto: NewRoomDto, res: Response) {
+    try {
+      await this.prisma.room.create({
+        data: {
+          hotelId,
+          ...dto,
+        },
+      });
+      return this.resHandler.requestSuccessful({
+        res,
+        message: 'Room added successfully',
+      });
+    } catch (err) {
+      return this.resHandler.serverError(res, 'Error creating new room');
+    }
+  }
 }
