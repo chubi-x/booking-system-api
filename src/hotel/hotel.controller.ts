bad3821e29f7262bc4fca3c003a81cd28221ac29
chubi-x <chubixax@protonmail.com>
2022-11-02 12:39:15 +0100

    feat: implement register hotel logic

--- src/hotel/hotel.controller.ts
+++ src/hotel/hotel.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
-
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
+import { RegisterHotelDto } from './dto';
+import { HotelService } from './hotel.service';
+import { Request, Response } from 'express';
 @Controller('hotels')
-export class HotelController {}
+export class HotelController {
+  constructor(private hotelService: HotelService) {}
+  // create hotel
+  @Post('/register')
+  async registerHotel(
+    @Body() dto: RegisterHotelDto,
+    @Req() req: Request,
+    @Res() res: Response,
+  ) {
+    return await this.hotelService.registerHotel(dto, req, res);
+  }
+  // get hotel by id
+  // get all hotels
+  // update hotel details (name,address,phone,email)
+  // reset password
+
+  // create hotel room
+  // update hotel room
+  //  get all rooms by single hotel
+  // get all rooms (public)
+  // delete room
+}
--- src/hotel/hotel.service.ts
+++ src/hotel/hotel.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
-
+import { Request, Response } from 'express';
+import { PrismaService } from '../database/database.service';
+import * as argon from 'argon2';
+import { HelpersService } from '../helpers/helpers.service';
+import { RegisterHotelDto } from './dto';
 @Injectable()
-export class HotelService {}
+export class HotelService {
+  constructor(
+    private prisma: PrismaService,
+    private resHandler: HelpersService.ResponseHandler,
+  ) {}
+
+  async registerHotel(dto: RegisterHotelDto, req: Request, res: Response) {
+    try {
+      // check if hotel exists
+      const hotel = await this.prisma.hotel.findUnique({
+        where: { email: dto.email },
+      });
+      if (!hotel) {
+        const password = await argon.hash(dto.password);
+        const hotel = await this.prisma.hotel.create({
+          data: { ...dto, password },
+        });
+        req.session.hotelId = hotel.id;
+        return this.resHandler.requestSuccessful({
+          res,
+          message: 'Hotel registered successfully',
+        });
+      } else {
+        return this.resHandler.clientError(res, 'Hotel already registered');
+      }
+    } catch (err) {
+      return this.resHandler.serverError(res, 'Error registering hotel');
+    }
+  }
+}
