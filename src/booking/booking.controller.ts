807c7d7a52e2c8bd953525bc638292bc78989352
chubi-x <chubixax@protonmail.com>
2022-11-03 22:30:15 +0100

    feat: implement logic to create booking

--- src/booking/booking.controller.ts
+++ src/booking/booking.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Get,
+  Delete,
+  Res,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { BookingService } from './booking.service';
-
+import { Response } from 'express';
+import { AuthGuard } from '../guards';
+import { GetUser } from '../decorators';
+import { CreateBookingDto } from './dto';
 @Controller('bookings')
 export class BookingController {
   constructor(private bookingService: BookingService) {}
+
+  // AUTHORIZED USER
+  // create booking (POST)
+  @UseGuards(AuthGuard)
+  @Post('/new')
+  async createBooking(
+    @GetUser() userId: string,
+    @Body() dto: CreateBookingDto,
+    @Res() res: Response,
+  ) {
+    return await this.bookingService.createBooking(userId, dto, res);
+  }
+  // update booking (PATCH)
+  // get booking by id (GET)
+  // Get all bookings by user (GET)
+  // delete booking by Id (DELETE)
+  //delete all bookings made by user (DELETE)
 }
--- src/booking/booking.service.ts
+++ src/booking/booking.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Response } from 'express';
 import { PrismaService } from '../database/database.service';
 import { HelpersService } from '../helpers/helpers.service';
+import { CreateBookingDto } from './dto';
 
 @Injectable()
 export class BookingService {
@@ -8,4 +10,56 @@ export class BookingService {
     private prisma: PrismaService,
     private resHandler: HelpersService.ResponseHandler,
   ) {}
+
+  /**
+   * Create Booking Function
+   * @param userId User's Id
+   * @param dto Class containing booking details
+   * @param res Express Response Object
+   * @returns ResHandler
+   */
+  async createBooking(userId: string, dto: CreateBookingDto, res: Response) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { id: userId },
+      });
+      const room = await this.prisma.room.findUnique({
+        where: { id: dto.roomId },
+      });
+      if (user && room) {
+        if (dto.numberOfRooms < room.numberAvailable) {
+          // create booking
+          await this.prisma.booking.create({
+            data: {
+              ...dto,
+              cost: dto.numberOfRooms * room.price,
+              userId: user.id,
+              roomId: room.id,
+              hotelId: room.hotelId,
+            },
+          });
+          // update available room number
+          await this.prisma.room.update({
+            where: { id: room.id },
+            data: { numberAvailable: room.numberAvailable - dto.numberOfRooms },
+          });
+          //   return response
+          return this.resHandler.requestSuccessful({
+            res,
+            message: 'Booking created successfully',
+          });
+        } else {
+          return this.resHandler.clientError(
+            res,
+            'The number of available rooms is less than your desired booking',
+          );
+        }
+      } else {
+        return this.resHandler.clientError(res, 'User or room does not exist.');
+      }
+    } catch (err) {
+      console.log(err);
+      return this.resHandler.serverError(res, 'Error creating booking');
+    }
+  }
 }
